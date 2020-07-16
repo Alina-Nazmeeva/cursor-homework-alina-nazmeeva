6d5c8fca4f1d45328f201461a516d774d472b054
Alina Nazmeeva <ell122zmey@gmail.com>
2020-07-16 22:20:28 +0300

    some changes

--- 01-homework/01-homework.js
+++ 01-homework/01-homework.js
@@ -1,38 +1,51 @@
-const bananas = 15.678;
-const beef = 123.965;
-const chicken = 90.2345;
-console.log("Мax price: " + Math.max(bananas, beef, chicken));
-console.log("Мin price: " + Math.min(bananas, beef, chicken));
-const total = bananas + beef + chicken;
-console.log("Total price: " + total);
-const totalIntPrice = Math.floor(bananas) + Math.floor(beef) + Math.floor(chicken);
-console.log("Total price without coins: " + totalIntPrice);
-const totalPriceRoundToHundreds = Math.round(total/100) * 100;
-console.log("Total price rounded to hundreds: " + totalPriceRoundToHundreds);
-const totalPriceFloor = Math.floor(total);
-console.log("Is rounded total price an even? " + (totalPriceFloor%2 === 0));
-const change = 500 - total;
-console.log("Change for 500: " + change);
-const averagePrice = Math.round(total/3*100)/100;
-console.log("Average price: " + averagePrice);
-const discount = Math.round(Math.random()*100);
-console.log(`Discount: ${discount}%`);
-const amountPayable = total/100*(100 - discount);
-console.log(amountPayable);
-console.log("Amount payable: " + Math.round(amountPayable * 100)/100);
-const costPrice = total/2;
-const netProfit = amountPayable - total/2;
-console.log("Net Profit: " + netProfit);
+const bananaPrice = 15.678;
+const beefPrice = 123.965;
+const chickenPrice = 90.2345;
+const cash = 500;
 
-const advanced = `Максимальна ціна: ${Math.max(bananas, beef, chicken)}
-Мінімальна ціна: ${Math.min(bananas, beef, chicken)}
-Вартість всіх товарів: ${total} 
-Вартість всіх товарів без копійок: ${totalIntPrice}
-Сума товарів, округлена до сотень: ${totalPriceRoundToHundreds}
-Чи є сума всіх товарів (округлена в меншу сторону) парним числом? ${totalPriceFloor%2 === 0}
-Решта з 500: ${change}
-Середнє значення цін, округлене до другого знаку після коми: ${averagePrice}
-Випадкова знижка: ${discount}
-Сума до оплати, округлена до 2 знаків після коми: ${amountPayable}
-Чистий прибуток ${netProfit}`;
-console.log(advanced);
+document.writeln("Base<br>");
+
+document.writeln("Мax price: " + Math.max(bananaPrice, beefPrice, chickenPrice) + "<br>");
+document.writeln("Мin price: " + Math.min(bananaPrice, beefPrice, chickenPrice) + "<br>");
+
+const totalPrice = bananaPrice + beefPrice + chickenPrice;
+document.writeln("Total price: " + totalPrice + "<br>");
+
+const totalIntPrice = Math.floor(bananaPrice) + Math.floor(beefPrice) + Math.floor(chickenPrice);
+document.writeln("Total price without coins: " + totalIntPrice + "<br>");
+
+const totalPriceRoundToHundreds = Math.round(totalPrice/100) * 100;
+document.writeln("Total price rounded to hundreds: " + totalPriceRoundToHundreds + "<br>");
+
+const isEven = Math.floor(totalPrice)%2 === 0;
+document.writeln("Is rounded total price an even? " + isEven + "<br>");
+
+const change = cash - totalPrice;
+document.writeln("Change for 500: " + change + "<br>");
+
+const averagePrice = Math.round(totalPrice/3*100)/100;
+document.writeln("Average price: " + averagePrice + "<br>");
+
+const discount = Math.round(Math.random()*50);
+document.writeln("Discount: " + discount + "%<br>");
+
+const amountPayable = Math.round(totalPrice/100*(100 - discount)*100)/100;
+document.writeln("Amount payable: " + amountPayable + "<br>");
+
+const profit = Math.round((amountPayable - totalPrice/2)*100)/100;
+document.writeln("Net Profit: " + profit + "<br>");
+
+document.writeln("<br>Advanced<br>");
+
+const advancedTask = `Максимальна ціна: ${Math.max(bananaPrice, beefPrice, chickenPrice)}<br>
+Мінімальна ціна: ${Math.min(bananaPrice, beefPrice, chickenPrice)}<br>
+Вартість всіх товарів: ${totalPrice}<br>
+Вартість всіх товарів без копійок: ${totalIntPrice}<br>
+Сума товарів, округлена до сотень: ${totalPriceRoundToHundreds}<br>
+Чи є сума всіх товарів (округлена в меншу сторону) парним числом? ${isEven}<br>
+Решта з 500: ${change}<br>
+Середнє значення цін, округлене до другого знаку після коми: ${averagePrice}<br>
+Випадкова знижка: ${discount}%<br>
+Сума до оплати, округлена до 2 знаків після коми: ${amountPayable}<br>
+Чистий прибуток: ${profit}`;
+document.writeln(advancedTask);
