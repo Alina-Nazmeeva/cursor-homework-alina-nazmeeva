031befc4c626a6b75126429a5a6f42469900b26a
Alina Nazmeeva <ell122zmey@gmail.com>
2020-08-12 18:55:21 +0300

    9 homework added

---
+++ 09-homework/main.js
@@ -0,0 +1,11 @@
+const generateRandomColor = () => Array.from({length: 3}).map(() => Math.round(Math.random()*255));
+
+function generateBlocks(){
+    const blocks = document.querySelectorAll('.squares');
+    blocks.forEach(block => block.style.backgroundColor = `rgb(${generateRandomColor()})`);
+}
+
+function generateBlocksInterval(){
+    window.setInterval(generateBlocks, 1000);
+}
+generateBlocksInterval();
