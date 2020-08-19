57c3947d79a6497cd04647047f490f7811a64d08
Alina Nazmeeva <ell122zmey@gmail.com>
2020-08-19 23:33:59 +0300

    10 homework added

---
+++ 10-homework/main.js
@@ -0,0 +1,29 @@
+const keys = document.querySelectorAll('.key');
+const audioTracks = document.querySelectorAll('.audio');
+
+function highlight(){
+    keys.forEach((key) => key.classList.remove('highlighted'));
+    this.classList.add('highlighted');
+}
+
+function playAudio(){
+    audioTracks.forEach((track) => {
+        if(!track.paused){
+            track.pause();
+            track.load();
+        } 
+    });
+    document.getElementById('audio-' + this.getAttribute('id')).play();
+}
+
+keys.forEach((key) => {
+    key.addEventListener('click', highlight);
+    key.addEventListener('click', playAudio);
+});
+
+document.addEventListener('keypress', function(event){
+    if(document.getElementById(event.keyCode)){
+        highlight.call(document.getElementById(event.keyCode));
+        playAudio.call(document.getElementById(event.keyCode));
+    }
+});
