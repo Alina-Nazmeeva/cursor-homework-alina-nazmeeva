ed25cdb39a1cd9e83554d63ab832737175cd3c00
Alina Nazmeeva <ell122zmey@gmail.com>
2020-09-02 22:48:59 +0300

    13 homework added

---
+++ 13-homework/main.js
@@ -0,0 +1,32 @@
+function* createIdGenerator(){
+    let id = 0;
+    while(true){
+        yield ++id;
+    }
+}
+const idGenerator = createIdGenerator();
+const paragraph = document.getElementById("paragraph");
+paragraph.innerText = `Generated IDs: ${idGenerator.next().value}, ${idGenerator.next().value}, ${idGenerator.next().value}`;
+
+function* createFontGenerator(size){
+    let fontSize = size;
+    while(true){
+        change = yield fontSize;
+        if(change === "up"){
+            fontSize++;
+        }
+        if(change === "down"){
+            fontSize--;
+        }
+    }
+}
+const fontGenerator = createFontGenerator(parseInt(getComputedStyle(document.body).fontSize));
+console.log(`Start fontSize: ${fontGenerator.next().value}`);
+document.getElementById('up-button').addEventListener('click', () => {
+    document.body.style.fontSize = (fontGenerator.next("up").value).toString() + 'px';
+    console.log(`Current fontSize: ${fontGenerator.next().value}`);
+});
+upButton = document.getElementById('down-button').addEventListener('click', () => {
+    document.body.style.fontSize = (fontGenerator.next("down").value).toString() + 'px';
+    console.log(`Current fontSize: ${fontGenerator.next().value}`);
+});
