158f151f1307d7c5af2ef6089766fefb0276e99b
Alina Nazmeeva <ell122zmey@gmail.com>
2020-09-10 20:34:07 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cursor-homework-alina-nazmeeva/15-homework/precache-manifest.be607c9a8d35c348c36d0e3f87cbcc5d.js"
+  "/cursor-homework-alina-nazmeeva/precache-manifest.97556e8591e73c15fa81335bf0263d82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cursor-homework-alina-nazmeeva/15-homework/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cursor-homework-alina-nazmeeva/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
